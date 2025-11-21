ce775c747884f77d7dc090a2f728d0d479d9679c
barmichev <barmichev@gmail.com>
2025-11-21 23:24:33 +0300

    Minor improvements

--- home/models.py
+++ home/models.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from django.db import models
 from modelcluster.fields import ParentalKey
-
 from wagtail import blocks
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField, StreamField
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.models import Image
 from wagtail.models import Page, Orderable
 from wagtailcodeblock.blocks import CodeBlock
 
@@ -13,13 +15,19 @@ class HomePage(Page):
     """
     Заглавная страница блога.
     """
+
     intro = RichTextField(blank=True)
 
     def get_context(self, request, *args, **kwargs):
         """Уточнение контекста по выдаче информации."""
+
         context = super().get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
+        blogpages = (self
+                     .get_children()
+                     .live()
+                     .order_by('-first_published_at', '-id'))
         context['blogpages'] = blogpages
+
         return context
 
 
@@ -52,7 +60,20 @@ class ArticlePage(Page):
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body'),
+        'gallery_images',
     ]
+
+    def hero_image(self) -> Optional[Image]:
+        """ Возвращает главную иллюстрацию, при наличии """
+
+        gallery_item = self.gallery_images.first()  # noqa: F821
+        if gallery_item:
+            return gallery_item.image
+
+    def has_hero_image(self) -> bool:
+        """ Есть хотя бы одна главная иллюстрация записи? """
+
+        return self.gallery_images.exists()  # noqa: F821
 
 
 class ArticlePageGalleryImage(Orderable):
